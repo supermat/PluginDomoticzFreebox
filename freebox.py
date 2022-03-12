56938749a23bbdd5d5855f0a33fb9b93a01286dc
supermat <matthieu.spriet@gmail.com>
2022-03-12 17:05:30 +0100

    Connection detail /1024/8 pour Ko

--- freebox.py
+++ freebox.py
@@ -197,8 +197,8 @@ class FbxApp(FbxCnx):
         retour = {}
         try:
             connection = self.com( "connection/")
-            retour.update({str('rate_down'):str(connection["result"]["rate_down"]/1000)})
-            retour.update({str('rate_up'):str(connection["result"]["rate_up"]/1000)})
+            retour.update({str('rate_down'):str(connection["result"]["rate_down"]/1024/8)})
+            retour.update({str('rate_up'):str(connection["result"]["rate_up"]/1024/8)})
         except (urllib.error.HTTPError, urllib.error.URLError) as error:
             Domoticz.Error('La Freebox semble indisponible : '+ error.msg)
         except timeout:
